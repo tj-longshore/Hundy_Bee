1914b1f397d8271940c7760eb174d77599a63493
tj-longshore <76498230+tj-longshore@users.noreply.github.com>
2021-12-20 16:24:30 -0500

    Completed Hundy Bee
    Completed Hundy Bee JS project

--- js/site.js
+++ js/site.js
@@ -1,4 +1,56 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//get range values from The App page
+//Start / controller function
+function getValues(){
+    //get values from the page
+    let startingValue = document.getElementById("startingValue").value;
+    let endingValue = document.getElementById("endingValue").value;
+    //parse values into integers
+    startingValue = parseInt(startingValue);
+    endingValue = parseInt(endingValue);
+    
+
+    if(Number.isInteger(startingValue) && Number.isInteger(endingValue)){
+        //call generateNumbers
+        numbers = generateNumbers(startingValue, endingValue);
+        //call displayNumbers
+        displayNumbers(numbers);
+    } else {
+        alert("Error: Values Entered Must Be Integers")
+    }
+
+}
+
+//generate the numbers from the startingValue to endingValue
+//logic function(s)
+function generateNumbers(sValue, eValue){
+    let numbers = [];
+    for (let i = sValue; i <= eValue; i++){
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+//display the numbers and mark even numbers bold
+//display or view function
+function displayNumbers(numbers){
+
+    let templateRows = "";
+
+    for(let i = 0; i < numbers.length; i++){
+
+        let className = "even";
+
+        let number = numbers[i];
+
+        if(number % 2 === 0){
+            className = "even";
+        }
+        else {
+            className = "odd";
+        }
+        templateRows += `<tr class="${className}"><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
 }
